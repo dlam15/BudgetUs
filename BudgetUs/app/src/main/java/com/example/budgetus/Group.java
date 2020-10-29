ccb2e7e56a972003305cdd1440051f224ec94990
brose102 <brose7@binghamton.edu>
2020-10-28 21:20:13 -0400

    Created Group Class

---
+++ BudgetUs/app/src/main/java/com/example/budgetus/Group.java
@@ -0,0 +1,66 @@
+package com.example.budgetus;
+import java.util.*;
+
+public class Group {
+    private String groupName;
+    private final int groupID;
+    private Map<Integer, String> members = new HashMap<>();
+
+    public Group(String groupName, int groupID){
+        this.groupName = groupName;
+        this.groupID = groupID;
+    }
+
+    public boolean setGroup_name(String groupName) {
+        this.groupName = groupName;
+        return true;
+    }
+
+    public String getGroup_name() {
+        return groupName;
+    }
+
+    public boolean registerUser(int newUser, String status){
+        if(members.isEmpty ()){
+            members.put(newUser, status);
+            return true;
+        }
+        else{
+            if(!(members.containsKey (newUser))){
+                members.put(newUser, status);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean unregisterUser(int remUser){
+        if(members.isEmpty ()){
+            return false;
+        }
+        else{
+            if(!(members.containsKey (remUser))){
+                members.remove (remUser);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean changeStatus(int curUser, String status){
+        if(members.isEmpty ()){
+            return false;
+        }
+        else{
+            if(members.containsKey (curUser)){
+                for (Map.Entry<Integer, String> tmp : members.entrySet()){
+                    if(tmp.getKey() == curUser){
+                        tmp.setValue(status);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
