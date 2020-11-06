788e65802667b398d29d58057680ec7571315107
matt <mkyea1@binghamton.edu>
2020-11-05 19:47:38 -0500

    changed my mind and decided that the builder wasn't what I was looking for, removed it, trying possible solutions to a clean way of optional class variables

--- BudgetUs/app/src/main/java/com/example/budgetus/Budget.java
+++ BudgetUs/app/src/main/java/com/example/budgetus/Budget.java
@@ -10,23 +10,16 @@ public class Budget {
     /* I have a few options here- transactions have quite a few optional fields. Do we
      *  -have multiple versions of this function?
      *  -have multiple constructors in Transaction?
-     *  -check for nullptr and use this to not set values?
-     * Right now, I like the idea of builders. We have 2 values that are required, and everything else is optional.
-     * We add whatever we want while building. However, it can be a bit confusing at first when making a new object,
-     * so I'll have to document how to use it.
+     *  -check for null and use this to not set values?
      *
      * In any case, we'll add a transaction to the arraylist. We'll also make sure to update any running totals.
      */
     public void addTransaction(){
-        Transaction t1 = Transaction.newBuilder(20, "pizza")
-                .withDescription("gim pizza from nirchis")
-                .withCategory(Transaction.Category.FOOD.addTag("nirchi's pizza"))
-                .build();
+        Transaction t1  = new Transaction(1, "to matt", null, null, null, null);
 
         //note - category tag does work, but adding it is a bit hard to find -
         //so, when we call .withCategory, we'll always pass a Category enum, but addTag is optional
         //but it isn't obvious that this is an option
-        //probably will change, but try to avoid nested builders
 
         System.out.println(t1.getAmount());
         System.out.println(t1.getName());
@@ -34,6 +27,8 @@ public class Budget {
         System.out.println(t1.getDescription());
         System.out.println(t1.getCategory());
         System.out.println(t1.getCategory().getTag());
+
+        t1.setDateOfTransaction(2020, 05, 1, -1, -1);
     }
 
 }
--- BudgetUs/app/src/main/java/com/example/budgetus/Transaction.java
+++ BudgetUs/app/src/main/java/com/example/budgetus/Transaction.java
@@ -9,19 +9,25 @@ import java.util.Objects;
 /*
  * The most basic/smallest unit of our budgeting feature.
  * A transaction is some exchange between parties for some amount for some object.
+ *
+ * A key issue I'm having is optional variables. There are a number of fields the user should
+ * be allowed to leave blank. However, I want to make creating a transaction easy, and use the same
+ * constructor. Doing so means we'll be dealing with null or checking for some default value?
  */
 public class Transaction {
 
     //variables
 
-    //required
+    //required. All transactions will have these fields defined.
     private double amount;
     private String name;
 
-    //optional
-    private Image receipt;//have not looked at - Image class may not be best
-    private String description;
-    private Calendar dateOfTransaction;//date is depreciated
+
+
+    //optional. A transaction can define 0 to all of these fields and anywhere in between.
+    private Image receipt = null;//have not looked at - Image class may not be best
+    private String description = null;
+    private Calendar dateOfTransaction = null;//date is depreciated
     //should allow different levels:
     //month and year
     //month and week and year
@@ -47,6 +53,8 @@ public class Transaction {
     }
     private Category category;
 
+
+    //basic getters
     public double getAmount() {
         return amount;
     }
@@ -71,6 +79,43 @@ public class Transaction {
         return category;
     }
 
+
+    //basic setters
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReceipt(Image receipt) {
+        this.receipt = receipt;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //little different - make it easy to provide numberical date rather than create calendar object
+    //might not actually be any easier, I'll see
+    public void setDateOfTransaction(int year, int month, int day, int hour, int minute) {
+        Calendar date = Calendar.getInstance();
+        date.clear();
+        /*
+        if(minute == -1) date.set(year, month, day, hour, 0);
+        if(hour == null) date.set(year, mnullonth, day);
+        if(day == null) date.set(year, month);
+        else
+        */
+        date.set(year, month, day, hour, minute);
+        this.dateOfTransaction = dateOfTransaction;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
     public void test(){
         dateOfTransaction.getInstance();//have to init like this first
         dateOfTransaction.clear();
@@ -80,69 +125,28 @@ public class Transaction {
         System.out.println(category.getTag());
     }
 
-    private Transaction (Builder builder){
-        Objects.requireNonNull(builder.name);
-        Objects.requireNonNull(builder.amount);
-        name = builder.name;
-        amount = builder.amount;
-        receipt = builder.receipt;
-        description = builder.description;
-        dateOfTransaction = builder.dateOfTransaction;
-        category = builder.category;
+    /*
+     * Haven't decided what to do with this yet exactly, but I'd like to use the same constructor for making every transaction.
+     * Otherwise, we'd need one for every possible combo
+     * Or we could just set the required ones, and just add what we have after? But then making an object is somewhat annoying
+     */
+    public Transaction(double amount, String name,  Image receipt,  String description, Calendar date,  Category category){
+        this.amount = amount;
+        this.name = name;
+        if(receipt != null) this.receipt = receipt;
+        if(description != null) this.description = description;
+        if(date != null) this.dateOfTransaction = date;
+        if(category!=null) this.category = category;
     }
 
-    public static Builder newBuilder(double amount, String name){
-        return new Builder(amount, name);
-    }
 
-    static final class Builder{
-        private double amount;
-        private String name;
-        private Image receipt;
-        private String description;
-        private Calendar dateOfTransaction;
-        private Category category;
-
-        private Builder(double amt, String nm) {
-            this.name = nm;
-            this.amount = amt;
-        }
-
-        public Transaction.Builder withAmount(double amt){
-            amount = amt;
-            return this;
-        }
-
-        public Transaction.Builder withName(String nm){
-            name = nm;
-            return this;
-        }
-
-        public Transaction.Builder withReceipt(Image recpt){
-            receipt = recpt;
-            return this;
-        }
-
-        public Transaction.Builder withDescription(String des){
-            description = des;
-            return this;
-        }
-
-        public Transaction.Builder withDate(Calendar date){
-            dateOfTransaction = date;
-            return this;
-        }
-
-        public Transaction.Builder withCategory(Category c){
-            category = c;
-            return this;
-        }
-
-        public Transaction build(){
-            return new Transaction(this);
-        }
-
-
+    /* A possible option for optional variables. If we don't store null,
+     * and instead use some default value, we'd need checks like this to
+     * tell that the field has not been provided by the user.
+     */
+    public boolean checkValidityDate(){
+        //if(this.dateOfTransaction == somedefaultval) return false;
+        return true;
     }
 
 
