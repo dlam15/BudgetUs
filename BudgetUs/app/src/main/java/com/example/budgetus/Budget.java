05f28963a19e987a0a043615faae0be9348bfb55
matt <mkyea1@binghamton.edu>
2020-11-12 17:20:30 -0500

    more features for budget stuff - sort transactions, add images

--- BudgetUs/app/src/main/java/com/example/budgetus/Budget.java
+++ BudgetUs/app/src/main/java/com/example/budgetus/Budget.java
@@ -1,34 +1,257 @@
 package com.example.budgetus;
 
+import android.content.Context;
+import android.media.Image;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Budget {
 
-    private ArrayList<Transaction> listOfTransactions = new ArrayList<>();
+    private ArrayList<Transaction> listOfTransactions = new ArrayList<>();//might use a different data structure
+    private final double startingFunds;
+    private double remainingFunds;
+    Context c;
 
-
-    /* I have a few options here- transactions have quite a few optional fields. Do we
-     *  -have multiple versions of this function?
-     *  -have multiple constructors in Transaction?
-     *  -check for null and use this to not set values?
-     *
-     * In any case, we'll add a transaction to the arraylist. We'll also make sure to update any running totals.
+    /*
+     *  Constructor requires startingFunds as well as a context (needed for a bitmap function in Transaction, so it gets passed there)
      */
-    public void addTransaction(){
-        Transaction t1  = new Transaction(1, "to matt", null, null, null, null);
+    public Budget(double startingFunds, Context c){
+        this.startingFunds = this.remainingFunds = startingFunds;
+        this.c = c;
+    }
 
-        //note - category tag does work, but adding it is a bit hard to find -
-        //so, when we call .withCategory, we'll always pass a Category enum, but addTag is optional
-        //but it isn't obvious that this is an option
+    //can get, but shouldn't be allowed to set - we want to maintain valid record via transactions
+    public double getStartingFunds() { return startingFunds; }
+    public double getRemainingFunds() { return remainingFunds; }
 
-        System.out.println(t1.getAmount());
-        System.out.println(t1.getName());
-        System.out.println(t1.getDateOfTransaction());
-        System.out.println(t1.getDescription());
-        System.out.println(t1.getCategory());
-        System.out.println(t1.getCategory().getTag());
 
-        t1.setDateOfTransaction(2020, 05, 1, -1, -1);
+     /*
+     * Add a new transaction to the list of transactions. Last 4 parameters are optional. We also update the remaining funds after this transaction.
+     *
+     * @param amount the amount the transaction was for, can be negative (but I haven't decided when that should be used)
+     * @param name the name for the transaction we can use to identify it
+     * @param receipt OPTIONAL - path to a photo on the user's device, we use it to make a bitmap image attached to the transaction
+     * @param description OPTIONAL - description for transaction, can be used when name doesn't explain enough
+     * @param date OPTIONAL - date the transaction occurred on. Right now this is a Calendar object, I might accept String format and convert if that's easier for caller
+     * @param category OPTIONAL - category from category enum - will probably change how the enum works, basic pre-defined choice right now
+     */
+    public void addTransaction(double amount, String name, Uri receipt, String description, Calendar date, Transaction.Category category){
+        Transaction t = new Transaction(c, startingFunds, amount, name, receipt, description, date, category);
+        listOfTransactions.add(t);
+        remainingFunds = this.remainingFunds - amount;//necessary to update funds correctly
+    }
+
+
+    /*
+     *  We need to provide a way to search the list of transactions - user could have huge list, needs to search for something.
+     *  Haven't worked this out yet (how to tell what field they are searching by to avoid a slow search?),  but searchable fields
+     *  are amount, name, description, date, category for now.
+     *
+     *  I'll just return the Transaction object and assume user can easily click on a transaction to work with it,  add/modify vars, sort, run stats, etc.
+     *  I.e. front end has a way to display Transaction object.
+     *
+     * @param some searchable value
+     * @return the Transaction object matching this criteria (more than 1?)
+     */
+    public Transaction findTransaction(double amount){
+        return null;
+    }
+
+
+    /*
+     *  A way to update a transaction (probably to add an optional field).
+     *  Comes in with a transaction parameter - user can click on or we'll search for it.
+     *  I haven't figured this out yet either - I'm imagining 1 screen/menu to update a transaction, not individual pop ups for edit name, edit amount, etc
+     *  and then we just update anything that's not null or not changed?
+     *  Also, will these fields be null, or will they be the previous values? probably the previous, but then how do we remove a value - provide null as picture would
+     *  have to mean remove rather than don't update
+     */
+    public void modifyTransaction(Transaction transaction, double amount, String name,  Image receipt,  String description,  Calendar date, Transaction.Category category){
+        //if(amount >0) transaction.amount = amount; //this needs to be more thought out - could have negatives?
+        //if(name != null)
+    }
+
+
+    /*
+     * Methods for sorting arraylist by different fields. Each works pretty much the same:
+     *  -declare a nested class that implements a Comparator for Transactions
+     *  -override compare function to compare what field we want
+     *  -call Collections.sort with arraylist and comparator we made
+     */
+
+
+    /*
+     * Sort by amount of transaction, low to high.
+     */
+    public void sortTransactionsLowToHigh(){
+        class SortByAmountLowToHigh implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                return Integer.compare(((int) a.getAmount()), (int) b.getAmount());
+            }
+        }
+        SortByAmountLowToHigh amountComparator = new SortByAmountLowToHigh();
+        Collections.sort(listOfTransactions, amountComparator);
+    }
+
+    /*
+     * Sort by amount of transaction, high to low.
+     */
+    public void sortTransactionsHighToLow(){
+        class SortByAmountHighToLow implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                return Integer.compare(((int) b.getAmount()), (int) a.getAmount());
+            }
+        }
+        SortByAmountHighToLow amountComparator = new SortByAmountHighToLow();
+        Collections.sort(listOfTransactions, amountComparator);
+    }
+
+
+    /*
+     * Sort by name of transaction, in alphabetical order.
+     */
+    public void sortTransactionsNameAlphabetical(){
+        class SortByNameAlphabetical implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                return a.getName().compareTo(b.getName());
+            }
+        }
+        SortByNameAlphabetical amountComparator = new SortByNameAlphabetical();
+        Collections.sort(listOfTransactions, amountComparator);
+    }
+
+    /*
+     * Sort by name of transaction, in reverse alphabetical order.
+     */
+    public void sortTransactionsNameReverseAlphabetical(){
+        class SortByNameReverseAlphabetical implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                return b.getName().compareTo(a.getName());
+            }
+        }
+        SortByNameReverseAlphabetical amountComparator = new SortByNameReverseAlphabetical();
+        Collections.sort(listOfTransactions, amountComparator);
+    }
+
+    /* Now some optional ones - requires checking null
+     * I will treat null as undefined/empty, so it will always come last (after the sorted/valid stuff)
+     * For example in alphabetical order: A, B, null, null
+     * In reverse alphabetical order: B, A, null
+     * Although in both of those example the user will probably see a blank space/empty string rather than the word null.
+     * I tested most of these but want to double check null always comes after.
+     *
+     * I'm also thinking about comparing by a required field when we have 2 nulls, but I haven't done that yet.
+     */
+
+    /*
+     * Sort by description in alphabetical order.
+     */
+    public void sortTransactionsDescriptionAlphabetical(){
+        for(Transaction t: listOfTransactions) System.out.println(t.getDescription());
+        class SortByDescriptionAlphabetical implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                //what worked for me so far is to compare by the empty string multiply by -1 to get all nulls after all sorted values
+                if(a.getDescription() == null) return -1 * "".compareTo(b.getDescription());
+                if(b.getDescription() == null) return -1 * a.getDescription().compareTo("");
+                return a.getDescription().compareTo(b.getDescription());
+            }
+        }
+        SortByDescriptionAlphabetical amountComparator = new SortByDescriptionAlphabetical();
+        Collections.sort(listOfTransactions, amountComparator);
+        for(Transaction t: listOfTransactions) System.out.println(t.getDescription());
+    }
+
+
+    /*
+     * Sort by description in reverse alphabetical order.
+     */
+    public void sortTransactionsDescriptionReverseAlphabetical(){
+        for(Transaction t: listOfTransactions) System.out.println(t.getDescription());
+        class SortByDescriptionReverseAlphabetical implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                //same here, comparing with empty string will put nulls after everything else
+                if(a.getDescription() == null) return b.getDescription().compareTo("");
+                if(b.getDescription() == null) return "".compareTo(a.getDescription());
+                return b.getDescription().compareTo(a.getDescription());
+            }
+        }
+        SortByDescriptionReverseAlphabetical amountComparator = new SortByDescriptionReverseAlphabetical();
+        Collections.sort(listOfTransactions, amountComparator);
+        for(Transaction t: listOfTransactions) System.out.println(t.getDescription());
+    }
+
+
+    /*
+     * Sort by date in alphabetical order of old/past to new/future.
+     */
+    public void sortTransactionsDateOldToNew(){
+        class SortByDateOldToNew implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                //was having an issue with null, so I'm making a calendar with 0 as the year and working with that, puts null after everything else
+                Calendar zero = new GregorianCalendar(0, 0, 0);
+                if(a.getDateOfTransaction() == null) return (-1) * zero.compareTo(b.getDateOfTransaction());
+                if(b.getDateOfTransaction() == null) return (-1) * a.getDateOfTransaction().compareTo(zero);
+                return a.getDateOfTransaction().compareTo(b.getDateOfTransaction());
+            }
+        }
+        SortByDateOldToNew amountComparator = new SortByDateOldToNew();
+        Collections.sort(listOfTransactions, amountComparator);
+    }
+
+    /*
+     * Sort by date in alphabetical order of new/future to old/past.
+     */
+    public void sortTransactionsDateNewToOld(){
+        class SortByDateNewToOld implements Comparator<Transaction>{
+            @Override
+            public int compare(Transaction a, Transaction b){
+                //same as above, year 0 gives null after everything else (assuming we have no transactions from 0)
+                Calendar zero = new GregorianCalendar(0, 0, 0);
+                if(a.getDateOfTransaction() == null) return b.getDateOfTransaction().compareTo(zero);
+                if(b.getDateOfTransaction() == null) return zero.compareTo(a.getDateOfTransaction());
+                return b.getDateOfTransaction().compareTo(a.getDateOfTransaction());
+            }
+        }
+        SortByDateNewToOld amountComparator = new SortByDateNewToOld();
+        Collections.sort(listOfTransactions, amountComparator);
+    }
+
+
+    /*
+     * Trying to do a quick breakdown of transactions by category. Didn't test and don't know how we'd display.
+     *
+     * @return a map/key val pairs of category, percentage we could use to make a pie chart or something
+     */
+    public Map<String, Double> breakdownInfo(){
+        Map<String, Double> ret = new HashMap<>();
+        int currCategoryEntries=0;
+        for(Transaction.Category c: Transaction.Category.values()){
+            currCategoryEntries = 0;
+            for(Transaction t: listOfTransactions){
+                if(t.getCategory() != null && t.getCategory() == c) currCategoryEntries++;
+            }
+            ret.put(c.toString(), (double) (currCategoryEntries/listOfTransactions.size()));
+        }
+        return null;
     }
 
 }
--- BudgetUs/app/src/main/java/com/example/budgetus/Transaction.java
+++ BudgetUs/app/src/main/java/com/example/budgetus/Transaction.java
@@ -1,45 +1,34 @@
 package com.example.budgetus;
 
-import android.media.Image;
-
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import java.io.ByteArrayOutputStream;
 import java.util.Calendar;
-import java.util.Locale;
-import java.util.Objects;
 
 /*
- * The most basic/smallest unit of our budgeting feature.
- * A transaction is some exchange between parties for some amount for some object.
+ * The most basic/smallest unit of our budgeting features
+ * A transaction is some exchange between parties for some amount
+ * I might make this a nested class of Budget - Budget is probably the only class that will use Transaction
  *
- * A key issue I'm having is optional variables. There are a number of fields the user should
- * be allowed to leave blank. However, I want to make creating a transaction easy, and use the same
- * constructor. Doing so means we'll be dealing with null or checking for some default value?
+ * We have some optional fields here -things the user can provide if they want, but don't have to
+ * Right now, those include a picture of a receipt, a description, a category, and a date of transaction.
+ * I think I'll just have to return null when any of those haven't been defined, but I may use a @Nullable annotation
+ * to remind everyone else that when trying to access an optional field, a return value of null is valid and means
+ * that the object is not yet defined.
  */
 public class Transaction {
 
-    //variables
-
-    //required. All transactions will have these fields defined.
-    private double amount;
-    private String name;
 
 
-
-    //optional. A transaction can define 0 to all of these fields and anywhere in between.
-    private Image receipt = null;//have not looked at - Image class may not be best
-    private String description = null;
-    private Calendar dateOfTransaction = null;//date is depreciated
-    //should allow different levels:
-    //month and year
-    //month and week and year
-    //month and day and year
-    //m/d/y and hour
-    //m/d/y and hour and minute
-    //might be able to use Calendar View to take input
-
-    //basic for now, might expand later
-    //want to offer a number of categories that makes sense
-    //but also want to allow user to provide a tag to specify where needed
-    //or does this make no sense, and user should make their own categories
+    /*
+     * basic enum of categories for now, might expand later
+     * want to offer a number of categories that makes sense
+     * but also want to allow user to provide a tag to specify where needed
+     * or does this make no sense, and user should make their own categories? - still thinking about
+     */
     public enum Category {
         FOOD, FUNDRAISING, OTHER;
         private String tag;
@@ -51,21 +40,60 @@ public class Transaction {
 
         public String getTag(){return tag;}
     }
-    private Category category;
 
 
-    //basic getters
+    /*
+     * Variables. We have required fields and optional fields
+     */
+
+    //required - all transactions will have these fields defined
+    private double amount;
+    private String name;
+    //required, but we handle these
+    private double amountBefore;
+    private double amountAfter;
+    private Context c;
+
+    //optional - A transaction can define 0 to all of these fields
+
+    /*
+     * so for images, I found how to save bitmaps from local URI on user's device
+     * but I can't save bitmap as string and convert back for json
+     * but I can do so with byte array, which is what we're doing here/why we're saving that
+     *
+     * flow:
+     * - From front end: we obtain uri of image from user's phone -> convert to bitmap and save -> convert to byte array and save
+     * - from back end: we obtain byte[] from database and save -> create bitmap and save
+     */
+    private byte[] receiptByteArray;//the bitmap converted to a byte array for JSON storage
+    private Bitmap receipt = null;//the actual image we can display on front end
+    private String description = null;
+    private Category category = null;
+    private Calendar dateOfTransaction = null;//Date is depreciated
+    //should allow different levels?
+    //month and year
+    //month and week and year
+    //month and day and year
+    //m/d/y and hour
+    //m/d/y and hour and minute
+    //might be able to use Calendar View to take input
+
+    /*
+     * Getters
+     */
     public double getAmount() {
         return amount;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public Image getReceipt() {
+    //the actual bitmap image we can display on the front end
+    public Bitmap getReceipt() {
         return receipt;
     }
+
+    //the byte conversion of the bitmap we can store in the database
+    public byte[] getReceiptByteArray() { return receiptByteArray; }
 
     public String getDescription() {
         return description;
@@ -79,8 +107,13 @@ public class Transaction {
         return category;
     }
 
+    public double getAmountBefore() { return amountBefore; }
 
-    //basic setters
+    public double getAmountAfter() { return amountAfter; }
+
+    /*
+     * Setters
+     */
     public void setAmount(double amount) {
         this.amount = amount;
     }
@@ -89,22 +122,61 @@ public class Transaction {
         this.name = name;
     }
 
-    public void setReceipt(Image receipt) {
+    public void setDescription(String description) { this.description = description; }
+
+    public void setCategory(Category category) { this.category = category; }
+
+    //image updating setters- when we update 1, update all (bitmap and byte[])
+
+    /*
+    * We accept Uri from front end, but don't need to save it
+    * so we just create bitmap and call that setter
+    * @param uri - the path to the photo on the user's device, we use to create bitmap then don't need
+     */
+    public void setReceipt(Uri uri){
+        try{
+            setReceipt(ImageDecoder.decodeBitmap(ImageDecoder.createSource(c.getContentResolver(), uri)));
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    /*
+     * when we update bitmap, need to update byte array
+     *
+     * @param receipt the bitmap (actual image) to store
+     */
+    public void setReceipt(Bitmap receipt) {
         this.receipt = receipt;
+        ByteArrayOutputStream output = new ByteArrayOutputStream(this.receipt.getByteCount());
+        this.receipt.compress(Bitmap.CompressFormat.JPEG, 100, output);
+        this.receiptByteArray = output.toByteArray();
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+
+    /*
+     * when we update byte array, need to update bitmap
+     *
+     * @param receiptByteArray the image represented in bytes, we can decode to get image
+     */
+    public void setReceiptByteArray(byte[] receiptByteArray) {
+        this.receiptByteArray = receiptByteArray;
+        this.receipt = BitmapFactory.decodeByteArray(this.receiptByteArray, 0, this.receiptByteArray.length);
     }
 
-    //little different - make it easy to provide numberical date rather than create calendar object
-    //might not actually be any easier, I'll see
+    public void setDateOfTransaction(Calendar date) { this.dateOfTransaction = date; }
+
+
+    /* work in progress
+     * little different - make it easy to provide numerical date rather than create calendar object
+     * might not actually be any easier, I'll see
+    */
     public void setDateOfTransaction(int year, int month, int day, int hour, int minute) {
         Calendar date = Calendar.getInstance();
         date.clear();
         /*
         if(minute == -1) date.set(year, month, day, hour, 0);
-        if(hour == null) date.set(year, mnullonth, day);
+        if(hour == null) date.set(year, month, day);
         if(day == null) date.set(year, month);
         else
         */
@@ -112,37 +184,42 @@ public class Transaction {
         this.dateOfTransaction = dateOfTransaction;
     }
 
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public void test(){
-        dateOfTransaction.getInstance();//have to init like this first
-        dateOfTransaction.clear();
-        dateOfTransaction.set(2020,05,05);
-        category = Category.FOOD;
-        category.addTag("pizza");
-        System.out.println(category.getTag());
-    }
 
     /*
-     * Haven't decided what to do with this yet exactly, but I'd like to use the same constructor for making every transaction.
-     * Otherwise, we'd need one for every possible combo
-     * Or we could just set the required ones, and just add what we have after? But then making an object is somewhat annoying
+     * I decided that we'll use the same constructor no matter how many optional fields are defined.
+     * We can provide null for any of the last 4 parameters, but the first 4 must be provided.
+     * I probably don't need to check for null here because I'll just set as null, but its a good reminder that these can be null.
+     *
+     * I also require the startingAmount variable so that each transaction is a mini-record of the budget's funds overtime.
+     *
+     * @param c a context variable needed when calling some global functions (specifically a bitmap function here)
+     * @param startingAmount the funds in the budget when this transaction is made
+     * @param cost the amount the transaction was for, can be negative (but I haven't decided when that should be used)
+     * @param name the name for the transaction we can use to identify it
+     * @param receipt OPTIONAL - path to a photo on the user's device, we use it to make a bitmap image attached to the transaction
+     * @param description OPTIONAL - description for transaction, can be used when name doesn't explain enough
+     * @param date OPTIONAL - date the transaction occurred on. Right now this is a Calendar object, I might accept String format and convert if that's easier for caller
+     * @param category OPTIONAL - category from category enum - will probably change how the enum works, basic pre-defined choice right now
      */
-    public Transaction(double amount, String name,  Image receipt,  String description, Calendar date,  Category category){
-        this.amount = amount;
+    public Transaction(Context c, double startingAmount, double cost, String name, Uri receipt,  String description,  Calendar date,  Category category){
+        this.c = c;//need for bitmap stuff for some reason
+        this.amountBefore = startingAmount;
+        this.amountAfter = this.amountBefore - cost;//handles positive and negative
+        this.amount = cost;
         this.name = name;
-        if(receipt != null) this.receipt = receipt;
+        if(receipt!=null) setReceipt(receipt); //sets this.receipt and this.receiptByteArray, need to check for null
+        //don't need to check the rest because they're null already, but its a good reminder
+        //or I might set some flag for each one and use that to check if its undefined rather than checking for null?
         if(description != null) this.description = description;
         if(date != null) this.dateOfTransaction = date;
         if(category!=null) this.category = category;
     }
 
-
     /* A possible option for optional variables. If we don't store null,
      * and instead use some default value, we'd need checks like this to
      * tell that the field has not been provided by the user.
+     *
+     * But I'm not doing that so far and probably won't need to
      */
     public boolean checkValidityDate(){
         //if(this.dateOfTransaction == somedefaultval) return false;
